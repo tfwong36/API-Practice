7389ab87b5c968c92036f866bc1e9300c9e2173a
tfwong36 <tfwong36@gmail.com>
2021-12-07 18:07:16 +0800

    [API-Practice][Jason] - feat: implement findAll function and Employee related Classes

---
+++ src/main/java/com/tatp/restapi/EmployeeRepository.java
@@ -0,0 +1,21 @@
+package com.tatp.restapi;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+public class EmployeeRepository {
+    private List<Employee> employees = new ArrayList<>();
+
+    public EmployeeRepository(){
+        employees.add(new Employee(1, "Jason", 18, "male", 5));
+        employees.add(new Employee(2, "Jason", 18, "male", 5));
+    }
+
+    public List<Employee> findAll(){
+        return employees;
+    }
+}
