13dd2e7ea27e7cfd9c776e4e9b0517ddc999192d
tfwong36 <tfwong36@gmail.com>
2021-12-08 21:11:34 +0800

    [API-Practice][Jason] - feat: create and pass service test case should_return_all_companies_when_find_all_given_companies

--- src/test/java/com/tatp/restapi/service/CompanyServiceTest.java
+++ src/test/java/com/tatp/restapi/service/CompanyServiceTest.java
@@ -1,13 +1,27 @@
 package com.tatp.restapi.service;
 
+        import com.tatp.restapi.entity.Company;
+        import com.tatp.restapi.entity.Employee;
         import com.tatp.restapi.repository.CompanyRepository;
+        import com.tatp.restapi.repository.EmployeeRepository;
+        import org.junit.jupiter.api.BeforeEach;
+        import org.junit.jupiter.api.Test;
         import org.junit.jupiter.api.extension.ExtendWith;
         import org.mockito.InjectMocks;
         import org.mockito.Mock;
+        import org.springframework.beans.factory.annotation.Autowired;
         import org.springframework.test.context.junit.jupiter.SpringExtension;
+        import org.springframework.test.web.servlet.MockMvc;
+        import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+        import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+        import java.util.ArrayList;
+        import java.util.List;
+
+        import static org.hamcrest.Matchers.hasSize;
         import static org.junit.jupiter.api.Assertions.assertEquals;
         import static org.mockito.ArgumentMatchers.any;
+        import static org.mockito.BDDMockito.given;
         import static org.mockito.Mockito.verify;
 
 @ExtendWith(SpringExtension.class)
@@ -15,7 +29,21 @@ public class CompanyServiceTest {
     @Mock
     CompanyRepository companyRepository;
     @InjectMocks
-    CompanyServiceTest companyServiceTest;
+    CompanyService companyService;
 
+    @Test
+    void should_return_all_companies_when_find_all_given_companies() {
+        //given
+        List<Company> companies = new ArrayList<>();
+        companies.add(new Company(1,"Spring", null));
+        given(companyRepository.findAll())
+                .willReturn(companies);
+
+        //when
+        List<Company> actual = companyService.findAll();
+
+        //then
+        assertEquals(companies, actual);
+    }
 
 }
