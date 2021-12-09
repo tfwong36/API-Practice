f82e135c257909d2354a6f634a1c092affda6a2b
tfwong36 <tfwong36@gmail.com>
2021-12-09 16:57:15 +0800

    [API-practice][Jason]-feat: use mongo db in company and created related interface and classes

--- src/main/java/com/tatp/restapi/service/CompanyService.java
+++ src/main/java/com/tatp/restapi/service/CompanyService.java
@@ -2,28 +2,34 @@ package com.tatp.restapi.service;
 
 import com.tatp.restapi.entity.Company;
 import com.tatp.restapi.entity.Employee;
+import com.tatp.restapi.exception.NoCompanyFoundException;
 import com.tatp.restapi.repository.CompanyRepository;
+import com.tatp.restapi.repository.CompanyRepositoryMongo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CompanyService {
     private CompanyRepository companyRepository;
     private EmployeeService employeeService;
+    private CompanyRepositoryMongo companyRepositoryMongo;
 
-
-    public CompanyService(CompanyRepository companyRepository, EmployeeService employeeService) {
+    public CompanyService(CompanyRepository companyRepository, EmployeeService employeeService, CompanyRepositoryMongo companyRepositoryMongo) {
         this.companyRepository = companyRepository;
         this.employeeService = employeeService;
+        this.companyRepositoryMongo = companyRepositoryMongo;
     }
 
     public List<Company> findAll() {
-        return companyRepository.findAll();
+        return companyRepositoryMongo.findAll();
+//        return companyRepository.findAll();
     }
     public Company findById(String id) {
-        Company company = companyRepository.findById(id);
-        return company;
+        return companyRepositoryMongo.findById(id).orElseThrow(NoCompanyFoundException::new);
+//        Company company = companyRepository.findById(id);
+//        return company;
     }
 
     public List<Employee> findEmployeesByCompanyId(String id) {
@@ -31,23 +37,31 @@ public class CompanyService {
     }
 
     public List<Company> findByPage(Integer page, Integer pageSize) {
-        return companyRepository.findByPage(page, pageSize);
+        return companyRepositoryMongo.findAll().stream()
+                .skip((long)page * pageSize)
+                .limit(pageSize)
+                .collect(Collectors.toList());
+//        return companyRepository.findByPage(page, pageSize);
     }
 
     public Company create(Company company) {
-        return companyRepository.create(company);
+        return companyRepositoryMongo.save(company);
+//        return companyRepository.create(company);
     }
 
     public void remove(String id) {
-        Company company = companyRepository.findById(id);
-        companyRepository.remove(id);
+        companyRepositoryMongo.deleteById(id);
+//        Company company = companyRepository.findById(id);
+//        companyRepository.remove(id);
     }
 
     public Company edit(String id, Company updatedCompany){
-        Company company = companyRepository.findById(id);
+        Company company = companyRepositoryMongo.findById(id).orElseThrow(NoCompanyFoundException::new);
+//        Company company = companyRepository.findById(id);
         if (updatedCompany.getName() != null)
             company.setName(updatedCompany.getName());
-        return companyRepository.save(id, company);
+        return companyRepositoryMongo.save(company);
+//        return companyRepository.save(id, company);
     }
 
 }
