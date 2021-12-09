b5870409e5ab2385011ea16952370051dc54bf71
tfwong36 <tfwong36@gmail.com>
2021-12-09 17:00:02 +0800

    [API-practice][Jason]-refactor: delete unused variable in service classes

--- src/main/java/com/tatp/restapi/service/CompanyService.java
+++ src/main/java/com/tatp/restapi/service/CompanyService.java
@@ -12,24 +12,19 @@ import java.util.stream.Collectors;
 
 @Service
 public class CompanyService {
-    private CompanyRepository companyRepository;
     private EmployeeService employeeService;
     private CompanyRepositoryMongo companyRepositoryMongo;
 
-    public CompanyService(CompanyRepository companyRepository, EmployeeService employeeService, CompanyRepositoryMongo companyRepositoryMongo) {
-        this.companyRepository = companyRepository;
+    public CompanyService(EmployeeService employeeService, CompanyRepositoryMongo companyRepositoryMongo) {
         this.employeeService = employeeService;
         this.companyRepositoryMongo = companyRepositoryMongo;
     }
 
     public List<Company> findAll() {
         return companyRepositoryMongo.findAll();
-//        return companyRepository.findAll();
     }
     public Company findById(String id) {
         return companyRepositoryMongo.findById(id).orElseThrow(NoCompanyFoundException::new);
-//        Company company = companyRepository.findById(id);
-//        return company;
     }
 
     public List<Employee> findEmployeesByCompanyId(String id) {
@@ -41,27 +36,21 @@ public class CompanyService {
                 .skip((long)page * pageSize)
                 .limit(pageSize)
                 .collect(Collectors.toList());
-//        return companyRepository.findByPage(page, pageSize);
     }
 
     public Company create(Company company) {
         return companyRepositoryMongo.save(company);
-//        return companyRepository.create(company);
     }
 
     public void remove(String id) {
         companyRepositoryMongo.deleteById(id);
-//        Company company = companyRepository.findById(id);
-//        companyRepository.remove(id);
     }
 
     public Company edit(String id, Company updatedCompany){
         Company company = companyRepositoryMongo.findById(id).orElseThrow(NoCompanyFoundException::new);
-//        Company company = companyRepository.findById(id);
         if (updatedCompany.getName() != null)
             company.setName(updatedCompany.getName());
         return companyRepositoryMongo.save(company);
-//        return companyRepository.save(id, company);
     }
 
 }
--- src/main/java/com/tatp/restapi/service/EmployeeService.java
+++ src/main/java/com/tatp/restapi/service/EmployeeService.java
@@ -13,44 +13,36 @@ import java.util.stream.Collectors;
 @Service
 public class EmployeeService {
     private EmployeeRepository employeeRepository;
-    private CompanyRepository companyRepository;
     private EmployeeRepositoryMongo employeeRepositoryMongo;
 
-    public EmployeeService(EmployeeRepository employeeRepository, CompanyRepository companyRepository, EmployeeRepositoryMongo employeeRepositoryMongo) {
+    public EmployeeService(EmployeeRepository employeeRepository, EmployeeRepositoryMongo employeeRepositoryMongo) {
         this.employeeRepository = employeeRepository;
-        this.companyRepository = companyRepository;
         this.employeeRepositoryMongo = employeeRepositoryMongo;
     }
 
     public List<Employee> findAll() {
         return employeeRepositoryMongo.findAll();
-//        return employeeRepository.findAll();
     }
 
     public Employee edit(String id, Employee updatedEmployee) {
         Employee employee = employeeRepositoryMongo.findById(id).orElseThrow(NoEmployeeFoundException::new);
-        //Employee employee = employeeRepository.findById(id);
         if (updatedEmployee.getAge() != null && !updatedEmployee.getAge().equals(0))
             employee.setAge(updatedEmployee.getAge());
         if (updatedEmployee.getSalary() != null && !updatedEmployee.getSalary().equals(0))
             employee.setSalary(updatedEmployee.getSalary());
         return employeeRepositoryMongo.save(employee);
-//        return employeeRepository.save(id, employee);
     }
 
     public Employee findById(String id) {
-//        return employeeRepositoryMongo.findById(id).orElseThrow(NoEmployeeFoundException::new);
         return employeeRepository.findById(id);
     }
 
     public List<Employee> findByGender(String gender) {
         return employeeRepositoryMongo.findByGender(gender);
-//        return employeeRepository.findByGender(gender);
     }
 
     public Employee create(Employee employee) {
         return employeeRepositoryMongo.save(employee);
-//        return employeeRepository.create(employee);
     }
 
     public List<Employee> findByPage(Integer page, Integer pageSize) {
@@ -58,12 +50,10 @@ public class EmployeeService {
                 .skip((long)page * pageSize)
                 .limit(pageSize)
                 .collect(Collectors.toList());
-//        return employeeRepository.findByPage(page, pageSize);
     }
 
     public void remove(String id) {
         employeeRepositoryMongo.deleteById(id);
-//        return employeeRepository.remove(id);
     }
 
     public List<Employee> getEmployeesByCompanyID(String companyID){
